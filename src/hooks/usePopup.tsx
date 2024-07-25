aa68a447b4566fe3b53b74b98dda7e9616b757cd
starr-starr <2654109566@qq.com>
2024-07-25 22:31:22 +0800

    first commit

---
+++ src/hooks/usePopup.tsx
@@ -0,0 +1,38 @@
+import type { ReactNode } from 'react'
+import { useState } from 'react'
+import ReactDOM from 'react-dom'
+import { Popup } from '../components/Popup'
+import { rootDiv } from '../main'
+
+type Options = {
+  initVisible?: boolean
+  children: ReactNode
+  position?: 'bottom' | 'center'
+  zIndex?: string
+}
+
+const divv = document.getElementById('divv') as HTMLElement
+
+export const usePopup = (options: Options) => {
+  const { initVisible = false, children, position, zIndex } = options
+  const [visible, setVisible] = useState(initVisible)
+  const popup = ReactDOM.createPortal(
+    <Popup zIndex={zIndex} visible={visible} position={position}
+      onClickMask={() => setVisible(false)} >
+      {children}
+    </Popup>,
+      divv
+  )
+  return {
+    popup,
+    show() {
+      setVisible(true)
+    },
+    hide() {
+      setVisible(false)
+    },
+    toggle() {
+      setVisible(!visible)
+    }
+  }
+}
